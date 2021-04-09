f11243f1f7fb352b5ff5dc19bd42fa649fc03bcb
MarianS <marian.s@web.de>
2021-04-09 13:35:58 +0200

    fixed npm.sh, added Test component

--- src/browser/components/App.tsx
+++ src/browser/components/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
+import Test from './Test';
 
 const App = () => (
-    <h1>hello world</h1>
+    <h1>hello world ! <Test /></h1>
 );
 
 export default hot(App);
---
+++ src/browser/components/Test.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Test = () => (
+    <span>123</span>
+);
+
+export default Test;
